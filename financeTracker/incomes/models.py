4726e0a4eced1b70433d375802e3a8f1066c113e
Maksim Petrushin <petrushin.maksim001@gmail.com>
2023-07-13 08:35:18 -0400

    added a db for incomes. also an early stage commit

--- financeTracker/incomes/models.py
+++ financeTracker/incomes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime    
 
 # Create your models here.
+
+class Income(models.Model):
+    incAmount = models.FloatField(default=0)
+    description = models.CharField(max_length=512)
+    category = models.CharField(max_length=64)
+    incDate = models.DateField(default=datetime.now(), blank=True)
+
+    def __str__(self):
+        return f"ID: {self.id} | AMOUNT: ${self.incAmount}| CATEGORY: {self.category} | DATE: {self.incDate} | DESCRIPTION: {self.description} | "
--- financeTracker/incomes/views.py
+++ financeTracker/incomes/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Income
 
 # Create your views here.
 
 def index(request):
-    return render(request, "incomes/index.html")
+    return render(request, "incomes/index.html",{
+        "incomes": Income.objects.all()
+    })
+
 
 def greet(request, name):
     return render(request, "incomes/greet.html",{
