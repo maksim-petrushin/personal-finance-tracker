6ef338ecbf33eee913f3680ec5bd845287a65e54
Maksim Petrushin <petrushin.maksim001@gmail.com>
2023-07-13 22:02:22 -0400

    added one to many relationship between incomes and users

--- financeTracker/incomes/models.py
+++ financeTracker/incomes/models.py
@@ -2,13 +2,19 @@ from django.db import models
 from datetime import datetime    
 
 # Create your models here.
-
-class Income(models.Model):
-    incAmount = models.FloatField(default=0)
-    description = models.CharField(max_length=512)
-    category = models.CharField(max_length=64)
-    incDate = models.DateField(default=datetime.now(), blank=True)
+class AppUsers(models.Model):
+    first= models.CharField(max_length=64, blank=False)
+    last = models.CharField(max_length=64,blank=False)
 
     def __str__(self):
-        return f"ID: {self.id} | AMOUNT: ${self.incAmount}| CATEGORY: {self.category} | DATE: {self.incDate} | DESCRIPTION: {self.description} | "
+        return f"name: {self.first} {self.last}"
+
+
+class Income(models.Model): 
+    person = models.ForeignKey(AppUsers, on_delete=models.PROTECT, related_name="pluses", blank=False, null=True)
+    incAmount = models.FloatField(default=0)
+    category = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"ID: {self.id} | AMOUNT: ${self.incAmount}| CATEGORY: {self.category}"
     
