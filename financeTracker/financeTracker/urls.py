6a24ecd2eb181c158938ff06e83cc94e19924078
Maksim Petrushin <petrushin.maksim001@gmail.com>
2023-07-16 22:40:38 -0400

    added bootstrap and crispy

--- financeTracker/financeTracker/urls.py
+++ financeTracker/financeTracker/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("incomes.urls")),
-    path('users/', include("users.urls"))
+    path('users/', include("users.urls")),
+    path('', include('django.contrib.auth.urls'))
 ]
