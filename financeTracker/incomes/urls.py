fe5b9c27e88d48111f298e4c00409614f2caa8b1
Maksim Petrushin <petrushin.maksim001@gmail.com>
2023-07-16 23:58:16 -0400

    added login and sign-up logic

--- financeTracker/incomes/urls.py
+++ financeTracker/incomes/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index1, name="index1"),
+    path("home", views.index1, name="index1"),
     path("<int:user_id>", views.user, name="user"),
     path("sign-up", views.sign_up, name="sign-up")
 
