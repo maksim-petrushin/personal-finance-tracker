04c965f151db4f98cb444242594237667f127665
Maksim Petrushin <petrushin.maksim001@gmail.com>
2023-07-18 23:27:28 -0400

    added date picker to transaction date field in the form

--- financeTracker/incomes/forms.py
+++ financeTracker/incomes/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Income
+from django.db import models
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required =True)
@@ -14,4 +18,5 @@ class PostIncome(forms.ModelForm):
     class Meta:
         model = Income
         fields = ["incAmount","transaction_category", "transaction_date"]
-
+        widgets = {'transaction_date' : DateInput()}
+    
